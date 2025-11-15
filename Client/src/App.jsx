c5c8b2f8ce44e787c71661a0ed0e7a4c10d2b755
hareem09 <hareemmohsin93@gmail.com>
2025-11-15 22:21:43 +0500

    updated

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,15 +7,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <>
-   <form action="" method="post">
-    <label htmlFor="username">Username:</label>
-    <input type="text" id="username" name="username" />
-    <label htmlFor="email">Email:</label>
-    <input type="email" id="email" name="email" />
-    <input type="submit" value="Submit" />
+  <>
+   <form action="/submit" method="post">
+    <label htmlFor="name">Name:</label>
+    <input type="text" id="name" name="name" required />
+    <button type="submit">Submit</button>
    </form>
-   </>
+  </>
   )
 }
 
