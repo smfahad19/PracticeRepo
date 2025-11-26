e06c3aeef22b11057621349344ace96f90b408f5
Furqan-Ali-2004 <furqangraphy2004@gmail.com>
2025-11-26 21:54:01 +0500

    some changess

--- Server/index.js
+++ Server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const dashboardRouter = require("./modules/dashboard");
 
 app.use("/api", dashboardRouter);
+app.use("/api/canddate", candidatedataRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
